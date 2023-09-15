061234ad5148c667b83ee91cdc2eaa02f7a04837
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2023-09-15 04:17:02 +0300

    Using Class Based View

---
+++ pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# from django.template import loader
+
+# Class-Based Views
+# Generic Class-Based Views
+# Function-Based views
+"""
+def homePageView(request):
+    template = loader.get_template("pages/home.html")
+    return HttpResponse(template.render())
+"""
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
